3f9974783792ffb6efce7465179da09871a66a05
LuisJann <giannoneluigi10@gmail.com>
2023-01-19 20:39:17 +0100

    add AppMain, AppCard, AppHeader  print data to Api

---
+++ src/components/AppCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name:"AppCard",
+    data(){
+        return{
+            contentMaxLength: 50,
+        }
+    },
+    props:{
+        project: Object,
+    },
+    methods: {
+        contentPreview(text) {
+            if (text && text.length > this.contentMaxLength) {
+                return text.substr(0, this.contentMaxLength) + '...';
+            }
+            return text;
+        }
+    }
+}
+</script>
+
+<template>
+<div class="card mb-3" >
+    <div class="card-body mb-3 text-center">
+        <h5 class="card-title">{{project.title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{project.slug}}</h6>
+        <p class="card-text">{{contentPreview(project.content)}}</p>
+        <a class="btn btn-primary" href="">Leggi di più</a>
+    </div>
+</div>
+</template>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+import AppCard from './AppCard.vue';
+
+export default {
+    name: "AppMain",
+    components:{
+        AppCard
+    },
+    data(){
+    return{
+      projects:[],
+    }
+  },
+  created(){
+    this.getProjects();
+  },
+  methods:{
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        this.projects = resp.data.results;
+      })
+    },
+  }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="text-center">I Progetti</h2>
+        <div class="row justify-content-center">
+            <div class="col-9">
+                <AppCard v-for="project in projects" :key="project.id"  :project="project"/>
+            </div>
+        </div>
+    </div>
+</template>
+
